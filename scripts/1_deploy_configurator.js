65fd5d0b116a2ef4402f4f78f43e6dd38b435ff3
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2021-08-11 18:39:09 +0300

    chore(scripts): update deployment script

--- scripts/1_deploy_configurator.js
+++ scripts/1_deploy_configurator.js
@@ -6,9 +6,16 @@ async function deploy() {
   const addresses = await web3.eth.getAccounts();
   const [owner] = addresses;
   const configurator = await Configurator.new({ from: owner });
-  log(`1. configurator deployed at address: @address{${configurator.address}}`);
-  // log(`Token address: @token{${await configurator.token()}}`)
-  // log(`Sale address: @address{${await configurator.sale()}}`)
+  log(`1. Configurator deployed at address: @address{${configurator.address}}`);
+  await configurator.step1({ from: owner });
+  log(`2. Step1 complete. CommonSale deployed at address @address{${await configurator.saleAddress()}}`);
+  await configurator.step2({ from: owner });
+  log(`3. Step2 complete. Wallets:`)
+  for (let i = 0; i < 5; i++) {
+    log(`@address{${await configurator.walletAddresses(i)}}`)
+  }
+  await configurator.step3({ from: owner });
+  log(`4. step3 complete. Token address: @address{${await configurator.tokenAddress()}}`)
 }
 
 module.exports = async function main(callback) {
