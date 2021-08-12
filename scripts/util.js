60f7b02448a176cd69c847caa6619bb80ad8426c
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2021-08-12 21:07:21 +0300

    chore(scripts/util): add `logRevert` method

--- scripts/util.js
+++ scripts/util.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 function logger(network) {
   let prefix;
+  
   switch (network) {
+    case 'kovan': prefix = 'https://kovan.etherscan.io'; break;
     case 'ropsten': prefix = 'https://ropsten.etherscan.io'; break;
     default: prefix = 'https://etherscan.io';
   }
-  return (text) => {
+  
+  const log = (text) => {
     const result = text
       .replace(/@address{(.+?)}/g, `${prefix}/address/$1`)
       .replace(/@token{(.+?)}/g, `${prefix}/address/$1`)
@@ -14,6 +17,18 @@ function logger(network) {
     console.log(result);
     fs.appendFileSync(`report.${network}.log`, `${result}\n`);
   }
+  
+  const logRevert = async (tryBlock, catchBlock) => {
+    try {
+      await tryBlock();
+    } catch (e) {
+      const txHash = Object.keys(e.data)[0]
+      const reason = e.data[txHash].reason
+      catchBlock(txHash, reason);
+    }
+  }
+  
+  return { log, logRevert }
 }
 
 module.exports = { logger };
