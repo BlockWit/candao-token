0334f300375cf7564a6d68bf3c170d6660f2ca64
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2021-10-15 18:06:31 +0300

    test(CommonSale): update test for common methods

--- test/commonsale.test.js
+++ test/commonsale.test.js
@@ -17,16 +17,16 @@ describe('CommonSale', async function () {
 
   beforeEach(async function () {
     STAGES = [
-      { start: await dateFromNow(1), end: await dateFromNow(8), bonus: 500, minInvestmentLimit: ether('0.03'), hardcap: ether('40000'), refETH: 5, refToken: 10 },
-      { start: await dateFromNow(9), end: await dateFromNow(11), bonus: 0, minInvestmentLimit: ether('0.03'), hardcap: ether('60000'), refETH: 5, refToken: 10 },
-      { start: await dateFromNow(11), end: await dateFromNow(13), bonus: 250, minInvestmentLimit: ether('0.03'), hardcap: ether('5000'), refETH: 5, refToken: 10 },
+      { start: await dateFromNow(1), end: await dateFromNow(8), bonus: 500, minInvestmentLimit: ether('0.03'), hardcap: ether('40000'), refETH: 5, refToken: 10, schedule: 0 },
+      { start: await dateFromNow(9), end: await dateFromNow(11), bonus: 0, minInvestmentLimit: ether('0.03'), hardcap: ether('60000'), refETH: 5, refToken: 10, schedule: 0 },
+      { start: await dateFromNow(11), end: await dateFromNow(13), bonus: 250, minInvestmentLimit: ether('0.03'), hardcap: ether('5000'), refETH: 5, refToken: 10, schedule: 0 }
     ];
     sale = await Sale.new();
     token = await Token.new('Candao', 'CDO', [await sale.address], [ether(String(SUPPLY))]);
     await sale.setWallet(ethWallet);
     await sale.setPrice(ether(String(PRICE)));
-    for (const { start, end, bonus, minInvestmentLimit, hardcap, refETH, refToken } of STAGES) {
-      await sale.addStage(start, end, bonus, minInvestmentLimit, 0, 0, hardcap, 0, 0, refETH, refToken);
+    for (const { start, end, bonus, minInvestmentLimit, hardcap, refETH, refToken, schedule } of STAGES) {
+      await sale.addStage(start, end, bonus, minInvestmentLimit, 0, 0, hardcap, 0, 0, refETH, refToken, schedule);
     }
     await sale.setToken(await token.address);
     await sale.transferOwnership(owner);
@@ -51,7 +51,7 @@ describe('CommonSale', async function () {
     const ethSent = ether('0.1');
     await sale.sendTransaction({ value: ethSent, from: buyer });
     const tokensExpected = ethSent.muln(PRICE * (100 + bonus) / 100);
-    const tokensReserved = (await sale.balances(buyer)).initialCDO;
+    const tokensReserved = (await sale.getAccountInfo(buyer)).initialCDO;
     expect(tokensReserved).to.be.bignumber.equal(tokensExpected);
   });
 
@@ -60,7 +60,7 @@ describe('CommonSale', async function () {
     await increaseDateTo(start);
     const ethSent = ether('99');
     await sale.sendTransaction({ value: ethSent, from: buyer });
-    const tokensReserved = (await sale.balances(buyer)).initialCDO;
+    const tokensReserved = (await sale.getAccountInfo(buyer)).initialCDO;
     expect(tokensReserved).to.be.bignumber.equal(hardcap);
   });
 
@@ -89,7 +89,7 @@ describe('CommonSale', async function () {
     const ethSent = ether('0.123');
     await sale.sendTransaction({ value: ethSent, from: buyer });
     const tokensExpected = ethSent.muln(PRICE * (100 + bonus) / 100);
-    const tokensReserved = (await sale.balances(buyer)).initialCDO;
+    const tokensReserved = (await sale.getAccountInfo(buyer)).initialCDO;
     expect(tokensReserved).to.be.bignumber.equal(tokensExpected);
   });
 
@@ -99,12 +99,12 @@ describe('CommonSale', async function () {
     const ethSent1 = ether('0.123');
     const ethSent2 = ether('0.456');
     await sale.sendTransaction({ value: ethSent1, from: referral });
-    const referralTokensBefore = (await sale.balances(referral)).initialCDO;
+    const referralTokensBefore = new BN((await sale.getAccountInfo(referral)).initialCDO);
     await sale.sendTransaction({ value: ethSent2, from: buyer, data: referral });
     const tokensExpected = ethSent2.muln(PRICE * (100 + bonus) / 100);
-    const tokensActual = (await sale.balances(buyer)).initialCDO;
+    const tokensActual = (await sale.getAccountInfo(buyer)).initialCDO;
     const referralTokensExpected = referralTokensBefore.add(tokensExpected.muln(refToken).divn(100));
-    const referralTokensActual = (await sale.balances(referral)).initialCDO;
+    const referralTokensActual = (await sale.getAccountInfo(referral)).initialCDO;
     expect(tokensActual).to.be.bignumber.equal(tokensExpected);
     expect(referralTokensActual).to.be.bignumber.equal(referralTokensExpected);
   });
@@ -115,7 +115,7 @@ describe('CommonSale', async function () {
     const ethSent = ether('0.123');
     await sale.buyWithETHReferral(referral, { value: ethSent, from: buyer });
     const referralETHExpected = ethSent.muln(refETH).divn(100);
-    const referralETHAccrued = (await sale.balances(referral)).balanceETH;
+    const referralETHAccrued = (await sale.getAccountInfo(referral)).balanceETH;
     expect(referralETHAccrued).to.be.bignumber.equal(referralETHExpected);
     await sale.activateWithdrawal({ from: owner });
     const referralBalanceBefore = new BN(await web3.eth.getBalance(referral));
@@ -127,7 +127,7 @@ describe('CommonSale', async function () {
   });
 
   it('should remove stage by index correctly', async function () {
-    await sale.removeStage(1, { from: owner });
+    await sale.deleteStage(1, { from: owner });
     const stage0 = await sale.stages(0);
     const stage1 = await sale.stages(1);
     const stage2 = await sale.stages(2);
