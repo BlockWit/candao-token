0fa5785a053b7970ab1f6243b5649587cd7c8cc1
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2021-11-01 21:18:35 +0300

    chore(scripts): update

--- scripts/1_deploy_configurator.js
+++ scripts/1_deploy_configurator.js
@@ -5,9 +5,9 @@ async function deploy () {
   const { log } = logger(await web3.eth.net.getNetworkType());
   const addresses = await web3.eth.getAccounts();
   const [owner] = addresses;
-  const configurator = await Configurator.new({ from: owner });
+  const configurator = await Configurator.new(owner, { from: owner });
   log(`Configurator deployed at address: @address{${configurator.address}}`);
-  log(`Sale address @address{${await configurator.sale()}}`);
+  log(`VestingWallet address @address{${await configurator.wallet()}}`);
   log(`Token address: @address{${await configurator.token()}}`);
 }
 
--- scripts/2_test_configuration.js
+++ scripts/2_test_configuration.js
@@ -3,134 +3,40 @@ const { logger } = require('./util');
 const { ether } = require('@openzeppelin/test-helpers');
 const Configurator = artifacts.require('Configurator');
 const Token = artifacts.require('CandaoToken');
-const Sale = artifacts.require('CommonSale');
+const VestingWallet = artifacts.require('VestingWallet');
 
 async function deploy () {
   const args = process.argv.slice(2);
   const CONFIGURATOR_ADDRESS = args[args.findIndex(argName => argName === '--configuratorAddress') + 1];
-  console.log(`Configurator address: ${CONFIGURATOR_ADDRESS}`);
+  console.log(`Using Configurator address: ${CONFIGURATOR_ADDRESS}`);
   const configurator = await Configurator.at(CONFIGURATOR_ADDRESS);
-  const SALE_ADDRESS = await configurator.sale();
-  const sale = await Sale.at(SALE_ADDRESS);
+  const WALLET_ADDRESS = await configurator.wallet();
+  const wallet = await VestingWallet.at(WALLET_ADDRESS);
   const TOKEN_ADDRESS = await configurator.token();
-  const token = await Token.at(TOKEN_ADDRESS);
 
   const { log, logRevert } = logger(await web3.eth.net.getNetworkType());
-  const [deployer, owner, buyer, referral, anotherAccount] = await web3.eth.getAccounts();
+  const [owner, user] = await web3.eth.getAccounts();
 
   await logRevert(async () => {
-    log(`CommonSale. Attempting to send Ether to the CommonSale contract before the sale starts. Should revert.`);
-    const tx = await web3.eth.sendTransaction({ from: buyer, to: SALE_ADDRESS, value: toWei('0.04', 'ether'), gas: '200000' });
-    log(`Result: successful tx: @tx{${tx.transactionHash}}`);
-  }, (txHash, reason) => {
-    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
-  });
-
-  await logRevert(async () => {
-    log(`CommonSale. Attempting to call 'updateStage' method from a non-owner account. Should revert.`);
-    const startTime = Math.floor(Date.now() / 1000).toString();
-    const tx = await sale.updateStage(0, startTime, '1629673200', '500', '30000000000000000', '18480000000000000000000000', 5, 10, { from: buyer });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  }, (txHash, reason) => {
-    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
-  });
-
-  await (async () => {
-    log(`CommonSale. Change the beginning time of the first stage.`);
-    const startTime = Math.floor(Date.now() / 1000);
-    const endTime = startTime + 24 * 3600;
-    const tx = await sale.updateStage(0, startTime.toString(), endTime.toString(), '500', '30000000000000000', '18480000000000000000000000', 5, 10, { from: owner });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await logRevert(async () => {
-    log(`CommonSale. Attempting to send less than the allowed amount of Eth. Should revert.`);
-    const tx = await web3.eth.sendTransaction({ from: buyer, to: SALE_ADDRESS, value: toWei('0.02', 'ether'), gas: '200000' });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  }, (txHash, reason) => {
-    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
-  });
-
-  await (async () => {
-    log(`CommonSale. Send 0.03 Eth from buyer's account.`);
-    const tx = await web3.eth.sendTransaction({ from: buyer, to: SALE_ADDRESS, value: toWei('0.03', 'ether'), gas: '200000' });
-    log(`Result: successful tx: @tx{${tx.transactionHash}}`);
-  })();
-
-  await (async () => {
-    log(`CommonSale. Send 0.09 Eth from buyer's account. Specify CDO referral.`);
-    const tx = await web3.eth.sendTransaction({ from: buyer, to: SALE_ADDRESS, value: toWei('0.09', 'ether'), data: referral, gas: '200000' });
-    log(`Result: successful tx: @tx{${tx.transactionHash}}`);
-  })();
-
-  await (async () => {
-    log(`CommonSale. Send 0.09 Eth from buyer's account. Specify ETH referral.`);
-    const tx = await sale.buyWithETHReferral(referral, { from: buyer, value: toWei('0.09', 'ether') });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await logRevert(async () => {
-    log(`CommonSale. Attempting to enable withdrawal from non-owner account. Should revert.`);
-    const tx = await sale.activateWithdrawal({ from: buyer });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  }, (txHash, reason) => {
-    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
-  });
-
-  await logRevert(async () => {
-    log(`CommonSale. Attempting to withdraw before withdrawal is enabled. Should revert`);
-    const tx = await sale.withdraw({ from: buyer });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  }, (txHash, reason) => {
-    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
-  });
-
-  await (async () => {
-    log(`CommonSale. Enable withdrawal.`);
-    const tx = await sale.activateWithdrawal({ from: owner });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await (async () => {
-    log(`CommonSale. Withdraw from buyer's account.`);
-    const tx = await sale.withdraw({ from: buyer });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await (async () => {
-    log(`CommonSale. Withdraw from referral's account.`);
-    const tx = await sale.withdraw({ from: referral });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await (async () => {
-    log(`Token. Transfer from buyer's account.`);
-    const balance = await token.balanceOf(buyer);
-    const tx = await token.transfer(anotherAccount, balance, { from: buyer });
-    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
-  })();
-
-  await logRevert(async () => {
-    log(`CommonSale. Attemping to set balance from non-owner account. Should revert`);
+    log(`VestingWallet. Attemping to set balance from non-owner account. Should revert`);
     const amount = ether('1');
-    const tx = await sale.setBalance(anotherAccount, amount, 0, 0, 2, { from: buyer });
+    const tx = await wallet.setBalance(user, amount, 0, 0, 2, { from: user });
     log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
   }, (txHash, reason) => {
     log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
   });
 
   await (async () => {
-    log(`CommonSale. Set balance. Should rewrite target account's balance.`);
+    log(`VestingWallet. Set balance. Should rewrite target account's balance.`);
     const amount = ether('1');
-    const tx = await sale.setBalance(anotherAccount, amount, 0, 0, 2, { from: owner });
+    const tx = await wallet.setBalance(7, user, amount, 0, { from: owner });
     log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
   })();
 
   await logRevert(async () => {
     log(`CommonSale. Attemping to add balances from non-owner account. Should revert`);
-    const amount1 = ether('1');
-    const amount2 = ether('2');
-    const tx = await sale.addBalances([buyer, anotherAccount], [amount1, amount2], 2, { from: buyer });
+    const amount = ether('1');
+    const tx = await wallet.addBalances(7, [user], [amount], { from: user });
     log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
   }, (txHash, reason) => {
     log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
@@ -138,9 +44,36 @@ async function deploy () {
 
   await (async () => {
     log(`CommonSale. add balances. Should increase target accounts by specified amounts.`);
-    const amount1 = ether('1');
-    const amount2 = ether('2');
-    const tx = await sale.addBalances([buyer, anotherAccount], [amount1, amount2], 2, { from: owner });
+    const amount = ether('1');
+    const tx = await wallet.addBalances(7, [user], [amount], { from: owner });
+    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
+  })();
+
+  await logRevert(async () => {
+    log(`CommonSale. Attempting to enable withdrawal from non-owner account. Should revert.`);
+    const tx = await wallet.activateWithdrawal({ from: user });
+    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
+  }, (txHash, reason) => {
+    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
+  });
+
+  await logRevert(async () => {
+    log(`CommonSale. Attempting to withdraw before withdrawal is enabled. Should revert`);
+    const tx = await wallet.withdraw({ from: user });
+    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
+  }, (txHash, reason) => {
+    log(`Result: Revert with reason '${reason}'. @tx{${txHash}}`);
+  });
+
+  await (async () => {
+    log(`CommonSale. Enable withdrawal.`);
+    const tx = await wallet.activateWithdrawal({ from: owner });
+    log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
+  })();
+
+  await (async () => {
+    log(`CommonSale. Withdraw from user's account.`);
+    const tx = await wallet.withdraw({ from: user });
     log(`Result: successful tx: @tx{${tx.receipt.transactionHash}}`);
   })();
 }
