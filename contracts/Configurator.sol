14397a195ec0795987c0f9b6e5d5dca9ecd1fc67
Arkadiy A.Davydov <jah.knows.me.well@gmail.com>
2021-11-01 19:01:02 +0300

    chore(Configurator): update contract

--- contracts/Configurator.sol
+++ contracts/Configurator.sol
@@ -11,68 +11,56 @@ contract Configurator is RecoverableFunds {
     using Address for address;
 
     CandaoToken public token;
-    VestingWallet public sale;
+    VestingWallet public wallet;
 
-    constructor() {
-        address OWNER_ADDRESS      = address(0x0b2cBc8a2D434dc16818B0664Dd81e89fAA9c3AC);
-
-        address[] memory accounts = new address[](7);
-        uint256[] memory supplies = new uint256[](7);
-
-        // casual eth accounts
-        accounts[0] = 0xe74b3Ba0e474eA8EE89da99Ef38889ED229e8782; // DAO 1
-        accounts[1] = 0xe74b3Ba0e474eA8EE89da99Ef38889ED229e8782; // DAO 2
-        accounts[2] = 0x539e3b4Cf63685cB2aE9adA4414335EB0B496Aec; // CONTENT MINING
-        accounts[3] = 0xe6Df6C96794063F2cC1Dc338908B2a04Ff29eBd4; // LIQUIDITY POOL
-        accounts[4] = 0x3aCe47351C48a4971b62176Ef3538C51397a0d5E; // FOUNDATION 1
-        accounts[5] = 0x3aCe47351C48a4971b62176Ef3538C51397a0d5E; // FOUNDATION 2
-
-        // supplies for casual eth accounts and sale contract
-        supplies[0] = 150_000_000 ether; // DAO 1
-        supplies[1] = 250_000_000 ether; // DAO 2
-        supplies[2] = 210_000_000 ether; // CONTENT MINING
-        supplies[3] = 191_500_000 ether; // LIQUIDITY POOL
-        supplies[4] =  50_000_000 ether; // FOUNDATION 1
-        supplies[5] = 100_000_000 ether; // FOUNDATION 2
-        supplies[6] = 548_500_000 ether; // SALE (seed, private sale, public sale, marketing, team, advisors)
-
-        // create sale
-        sale = new VestingWallet();
-        // stages
-
-        // withdrawal policies
+    constructor(address owner) {
+        // create wallet
+        wallet = new VestingWallet();
+        // set percent rate
+        wallet.setPercentRate(1000);
+        // groups
+        wallet.addGroup(1);
+        wallet.addGroup(2);
+        wallet.addGroup(3);
+        wallet.addGroup(4);
+        wallet.addGroup(5);
+        wallet.addGroup(6);
+        wallet.addGroup(7);
+        wallet.addGroup(8);
+        wallet.addGroup(9);
+        wallet.addGroup(10);
+        wallet.addGroup(11);
+        wallet.addGroup(12);
+        // vesting schedules
         // investors
-        sale.setVestingSchedule(1,  24 weeks, 104 weeks, 1 weeks, 40);
-        sale.setVestingSchedule(2,  12 weeks, 104 weeks, 1 weeks, 60);
-        sale.setVestingSchedule(3,  12 weeks, 104 weeks, 1 weeks, 60);
-        sale.setVestingSchedule(4,  12 weeks, 104 weeks, 1 weeks, 60);
-        sale.setVestingSchedule(5,   4 weeks, 104 weeks, 1 weeks, 100);
+        wallet.setVestingSchedule(1,  24 weeks, 104 weeks, 1 weeks, 40);
+        wallet.setVestingSchedule(2,  12 weeks, 104 weeks, 1 weeks, 60);
+        wallet.setVestingSchedule(3,  12 weeks, 104 weeks, 1 weeks, 60);
+        wallet.setVestingSchedule(4,  12 weeks, 104 weeks, 1 weeks, 60);
+        wallet.setVestingSchedule(5,   4 weeks, 104 weeks, 1 weeks, 100);
         // ecosystem
-        sale.setVestingSchedule(6,   4 weeks, 208 weeks, 1 weeks, 15);
-        sale.setVestingSchedule(7,   4 weeks, 208 weeks, 1 weeks, 10);
-        sale.setVestingSchedule(8,   0,       208 weeks, 1 weeks, 20);
+        wallet.setVestingSchedule(6,   4 weeks, 208 weeks, 1 weeks, 15);
+        wallet.setVestingSchedule(7,   4 weeks, 208 weeks, 1 weeks, 10);
+        wallet.setVestingSchedule(8,   0,       208 weeks, 1 weeks, 20);
         // internal
-        sale.setVestingSchedule(9,  10 weeks, 208 weeks, 1 weeks, 30);
-        sale.setVestingSchedule(10,  0,       208 weeks, 1 weeks, 50);
-        sale.setVestingSchedule(11,  0,       103 weeks, 1 weeks, 50);
-        sale.setVestingSchedule(12, 24 weeks, 104 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(9,  10 weeks, 208 weeks, 1 weeks, 30);
+        wallet.setVestingSchedule(10,  0,       208 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(11,  0,       103 weeks, 1 weeks, 50);
+        wallet.setVestingSchedule(12, 24 weeks, 104 weeks, 1 weeks, 50);
 
-        // accounts
-        sale.setBalance(0, 0x5219Cc08E635e9764c8014D984d60Fb7Ed200EEB, 150_000_000 ether, 0); // TEAM account
-        sale.setBalance(0, 0x9Ce19eF683c3dA57C19a874d214073f0Cd3B7F71,  10_000_000 ether, 0); // MARKETING 1 account
-        sale.setBalance(0, 0x9Ce19eF683c3dA57C19a874d214073f0Cd3B7F71,  40_000_000 ether, 0); // MARKETING 2 account
-        sale.setBalance(0, 0x9Ce19eF683c3dA57C19a874d214073f0Cd3B7F71,  50_000_000 ether, 0); // MARKETING 3 account
-        sale.setBalance(0, 0x05422B2b38ec652384F1b9dFA3487B2eeA58B544,  40_000_000 ether, 0); // ADVISORS account
-        accounts[6] = address(sale);
+        address[] memory addresses = new address[](1);
+        uint256[] memory amounts = new uint256[](1);
+
+        addresses[0] = address(wallet);
+        amounts[0] = 1_500_000_000 ether;
 
         // create token
-        require(accounts.length == supplies.length, "Configurator: wrong account array length");
-        token = new CandaoToken("Candao", "CDO", accounts, supplies);
-        token.transferOwnership(OWNER_ADDRESS);
+        token = new CandaoToken("Candao", "CDO", addresses, amounts);
+        token.transferOwnership(owner);
 
-        // finish sale configuration
-        sale.setToken(address(token));
-        sale.transferOwnership(OWNER_ADDRESS);
+        // finish wallet configuration
+        wallet.setToken(address(token));
+        wallet.transferOwnership(owner);
     }
 
 }
